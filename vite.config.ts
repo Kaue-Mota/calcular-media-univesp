3eae1046b27ce7cbf96f909ca6caae71259215c8
Kaue-Mota <pckaue123@gmail.com>
2026-04-13 21:59:24 -0300

    chore: add historyApiFallback to server and preview configurations

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  appType: 'spa',
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  },
 })
